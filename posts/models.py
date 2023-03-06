8caa6ac69215ef0810c13bc2f512fc18122b7eca
Danilo Cardoso <levitano@hotmail.com>
2023-03-06 17:28:53 -0300

    feat: create models and followers_friendships app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    class Meta:
+        ordering = ("id",)
+
+    description = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    private = models.BooleanField(default=False)
+    can_comment = models.BooleanField(default=True)
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
+    like = models.ForeignKey(
+        "likes.Like",
+        on_delete=models.CASCADE,
+        related_name="posts",
+        default=None,
+        null=True
+    )
+    comment = models.ForeignKey(
+        "comments.Comment",
+        on_delete=models.CASCADE,
+        related_name="posts",
+        default=None,
+        null=True
+    )
