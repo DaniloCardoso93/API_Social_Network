07a5c6f0a47e16ecd754482857d54879b182d77b
Garcia <garciasholding@gmail.com>
2023-03-07 16:46:31 -0300

    feat: comments route create, post, delete

--- comments/urls.py
+++ comments/urls.py
@@ -4,7 +4,5 @@ from rest_framework_simplejwt import views as jwt_views
 from . import views
 
 urlpatterns = [
-    # path("users/", views.UserView.as_view()),
-    # path("users/<int:user_id>/", views.UserDetailView.as_view()),
-    # path("users/login/", jwt_views.TokenObtainPairView.as_view()),
+    path("comments/<int:comment_id>/", views.CommentDetailView.as_view()),
 ]
