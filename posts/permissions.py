e59a685e9006bf3c8263e002c095324d48e4d844
unknown <gelanlucas@gmail.com>
2023-03-09 20:01:04 -0300

    update/permission

--- posts/permissions.py
+++ posts/permissions.py
@@ -2,13 +2,12 @@ from rest_framework import permissions
 from .models import Post
 from users.models import User
 from rest_framework.views import View
-import ipdb
-
+from django.forms.models import model_to_dict
 
 class PostPermission(permissions.BasePermission):
     def has_object_permission(self, request, view: View, obj: Post) -> bool:
         post_owner = User.objects.get(id=obj.user_id)
         if obj.private:
-            for user in post_owner.followers:
-                if user['id'] == request.user.id:
+            for user in list(post_owner.followers.all()):
+                if user.id == request.user.id:
                     return True
