f952ab3d1720fc80a42c851c2f4d45c6f5cbc711
Garcia <garciasholding@gmail.com>
2023-03-14 09:34:36 -0300

    hotfix: project settings for running in localhost

--- _social_network/settings.py
+++ _social_network/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["api-rede-social-production.up.railway.app", "0.0.0.0"]
+ALLOWED_HOSTS = ["api-rede-social-production.up.railway.app", "0.0.0.0", "localhost"]
 RAILWAY_STATIC_URL = os.getenv("RAILWAY_STATIC_URL")
 
 if RAILWAY_STATIC_URL:
